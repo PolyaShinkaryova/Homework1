77ba81ee89d8d648cc5d4723d16e229537bbf2f1
PolyaShinkaryova <shinkaryova.polina@yandex.ru>
2024-09-10 22:47:54 +0300

    Добавьте файлы проекта.

---
+++ Homework1/kotik.c
@@ -0,0 +1,12 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	puts("     ^_____^");
+	puts("   (  o   o  )");
+	puts("  ( ==  ^  == )");
+	puts("   )         (");
+	puts("  (           )");
+	puts(" ( (  )   (  ) )");
+	puts("(__(__)___(__)__)");
+}
